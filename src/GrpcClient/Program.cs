ac78e6eaf1651772f0397abebd6055e913c835dc
Mat <prevelm@gmail.com>
2023-12-17 15:08:54 +0100

    Establish grpc client/server communication.

--- src/GrpcClient/Program.cs
+++ src/GrpcClient/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace SampleGrpcClient;
+﻿using Grpc.Net.Client;
+using SampleGrpcServer;
+
+namespace SampleGrpcClient;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var uriBuilder = new UriBuilder
+        {
+            Host = "localhost",
+            Port = 5149
+        };
+
+        var channel = GrpcChannel.ForAddress(uriBuilder.Uri);
+
+        var client = new Greeter.GreeterClient(channel);
+
+        var count = 10;
+
+        for (int i = 0; i < count; i++)
+        {
+            var request = new HelloRequest
+            {
+                Name = $"Mat number: {i}"
+            };
+            Console.WriteLine($"Request: {request.Name}");
+            var response = await client.SayHelloAsync(request);
+            if (response == null)
+            {
+                Console.WriteLine("The response is null");
+            }
+            else
+            {
+                Console.WriteLine($"Response: {response.Message}");
+            }
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
     }
 }
